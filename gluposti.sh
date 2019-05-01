cfed142f4f2ba5059345c9768370cb221b864b7c
Marco Codic <trinem980@gmail.com>
2019-05-01 18:58:44 +0200

    Initial commit

---
+++ gluposti.sh
@@ -0,0 +1,36 @@
+
+mkdir ~/testing/gluposti
+
+
+
+
+basic-utils
+sudo apt-get update
+sudo apt-get upgrade
+
+git
+zsh
+ohmyzsh
+
+SHELL=/bin/zsh
+    /usr/bin/zsh
+
+
+echo set -o vi > ~/.inputrc
+
+
+
+
+vim
+mkdir .vim/bundle
+git clone vundle github
+starat vim with :PluginInstall
+
+
+ranger
+w3m
+youtube-dl
+sudo youtube-dl -U
+
+# last one
+sudo apt autoremove
